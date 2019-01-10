f039a382467bc1a21934cda7c502b87cb73638fa
Meirinaldo Júnior <meirinaldo.junior0@gmail.com>
2019-01-10 18:59:22 -0300

    remove files initials express, configuration routes initials

--- back/app.js
+++ back/app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./src/routes/index");
+var votesRouter = require("./src/routes/votes");
+var configRouter = require('./src/routes/config');
 
 var app = express();
 
@@ -19,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/votes", votesRouter);
+app.use('/config', configRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +37,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({
+    status: "ok",
+    version: "0.0.1"
+  });
 });
 
 module.exports = app;
